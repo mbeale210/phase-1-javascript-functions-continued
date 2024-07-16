4cf0e093223dd4ffe34f9495f95cad9d4d87cb26
Mark Beale <bealebeaverst@gmail.com>
2024-07-15 23:41:42 -0700

    Complete Lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+
+function saturdayFun(activity = "roller-skate") {
+  if (activity === "roller-skate") {
+    return "This Saturday, I want to roller-skate!";
+  } else {
+    return "This Saturday, I want to bathe my dog!";
+  }
+}
+function mondayWork(toDo = "go to the office"){
+    if(toDo === "go to the office"){
+        return "This Monday, I will go to the office.";
+    }
+    if(toDo = "work from home"){
+        return "This Monday, I will work from home.";
+    }
+}
+function wrapAdjective(symbol) {
+    return function(adjective) {
+        return `You are ${symbol}${adjective}${symbol}!`;
+    };
+}
+
 // code your solution here
